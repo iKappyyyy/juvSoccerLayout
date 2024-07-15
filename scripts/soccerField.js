c9b74511dfbf95f26b2e9c65fb425b4b396352ec
iKappyyyy <specialtimewithdaniel@gmail.com>
2024-07-15 11:43:42 +0300

    pretty much finished page 1

--- scripts/soccerField.js
+++ scripts/soccerField.js
@@ -1,11 +1,13 @@
 import layoutPositions from "./data/layoutPositions.js";
 
-const players = document.querySelectorAll('.js-player');
+export function updateField(currentLayout) {
+  const players = document.querySelectorAll('.js-player');
 
-const layoutPosition = layoutPositions['4-4-2'];
-players.forEach((player, index) => {
+  const layoutPosition = layoutPositions[currentLayout];
+  players.forEach((player, index) => {
   const playerId = `player${index + 1}`;
 
   player.style.gridRow = layoutPosition[playerId].row;
   player.style.gridColumn = layoutPosition[playerId].column;
 });
+}
